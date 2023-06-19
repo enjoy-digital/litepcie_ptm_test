6afca5781cb45bb0d2b4a4bb6f5b5f6f05a5c260
Florent Kermarrec <florent@enjoy-digital.fr>
2023-06-19 19:44:45 +0200

    target: Simplify design, expose more parameter and get back to MSI for now.

--- ocp_tap_timecard.py
+++ ocp_tap_timecard.py
@@ -84,12 +84,12 @@ class BaseSoC(SoCMini):
         "pcie_msi":       3, # Requires fixed mapping for MSI-X.
         "pcie_msi_table": 4, # Requires fixed mapping for MSI-X.
     }
-    def __init__(self, sys_clk_freq=100e6,
+    def __init__(self, sys_clk_freq=100e6, pcie_address_width=32, pcie_msi_type="msi",
         with_jtag_bone  = True,
         with_led_chaser = True,
         with_pcie       = False,
         with_smas       = False,
-        with_analyzer   = True,
+        with_analyzer   = False,
         **kwargs):
         platform = ocp_tap_timecard.Platform()
 
@@ -125,25 +125,13 @@ class BaseSoC(SoCMini):
             self.pcie_phy = S7PCIEPHY(platform, platform.request("pcie_x1"),
                 data_width = 64,
                 bar0_size  = 0x10_0000, # 1MB.
-                msi_type   = "msi-x"
+                msi_type   = pcie_msi_type,
             )
-            self.add_pcie(phy=self.pcie_phy, ndmas=1, address_width=32, msi_type="msi-x")
+            self.add_pcie(phy=self.pcie_phy, ndmas=1, address_width=pcie_address_width, msi_type=pcie_msi_type)
             # FIXME: Apply it to all targets (integrate it in LitePCIe?).
             platform.add_period_constraint(self.crg.cd_sys.clk, 1e9/sys_clk_freq)
             platform.toolchain.pre_placement_commands.append("reset_property LOC [get_cells -hierarchical -filter {{NAME=~*gtp_channel.gtpe2_channel_i}}]")
             platform.toolchain.pre_placement_commands.append("set_property LOC GTPE2_CHANNEL_X0Y5 [get_cells -hierarchical -filter {{NAME=~*gtp_channel.gtpe2_channel_i}}]")
-
-            # ICAP (For FPGA reload over PCIe).
-            from litex.soc.cores.icap import ICAP
-            self.icap = ICAP()
-            self.icap.add_reload()
-            self.icap.add_timing_constraints(platform, sys_clk_freq, self.crg.cd_sys.clk)
-
-            # Flash (For SPIFlash update over PCIe).
-            from litex.soc.cores.gpio import GPIOOut
-            from litex.soc.cores.spi_flash import S7SPIFlash
-            self.flash_cs_n = GPIOOut(platform.request("flash_cs_n"))
-            self.flash      = S7SPIFlash(platform.request("flash"), sys_clk_freq, 25e6)
 
         # Analyzer ---------------------------------------------------------------------------------
 
@@ -169,68 +157,6 @@ class BaseSoC(SoCMini):
                 csr_csv      = "analyzer.csv"
             )
 
-
-        # SMAs -------------------------------------------------------------------------------------
-        if with_smas:
-            # TODO:
-            # - Use IO primitives to increase IO freqs (up to 800Mz would be fine) and PCIe Gen2 X1
-            #   should be able to saturate it (~3.4Gbps of useful bandwidth).
-            # - Allow IO freq configuration (through PLL/MMCM and/or dividers).
-
-            class SMAIOs(LiteXModule):
-                def __init__(self, pcie_data_width=64, io_data_width=4):
-                    # Endpoints.
-                    self.sink   = stream.Endpoint([("data", pcie_data_width)])
-                    self.source = stream.Endpoint([("data", pcie_data_width)])
-
-                    # CSRs.
-                    self.control = CSRStorage(fields=[
-                        CSRField("in_en",  size=io_data_width, description="Input  enable control (1bit per SMA)."),
-                        CSRField("out_en", size=io_data_width, description="Output enable control (1bit per SMA)."),
-                    ])
-                    self.output  = CSRStorage(io_data_width, description="SMA Reg Output (1bit per SMA).")
-                    self.input   = CSRStatus(io_data_width,  description="SMA Reg Input  (1bit per SMA).")
-
-                    # # #
-
-                    # SMA Pads.
-                    sma_pads = [platform.request("sma", i) for i in range(io_data_width)]
-
-                    # SMA Buffer Control.
-                    for i in range(io_data_width):
-                        self.sync += sma_pads[i].dat_in_en.eq( self.control.fields.in_en)
-                        self.sync += sma_pads[i].dat_out_en.eq(self.control.fields.in_en)
-
-                    # SMA TX Reg, allow direct (and slow...) control of SMA IOs.
-                    for i in range(io_data_width):
-                        self.sync += If(self.output.storage[i],
-                            sma_pads[i].dat_out.eq(1)
-                        )
-
-                    # SMA TX Data Pipeline.
-                    self.tx_converter = tx_converter = stream.Converter(pcie_data_width, io_data_width)
-                    self.comb += self.sink.connect(self.tx_converter.sink)
-                    self.comb += self.tx_converter.source.ready.eq(1)
-                    for i in range(io_data_width):
-                        self.sync += If(tx_converter.source.valid & tx_converter.source.data[i],
-                            sma_pads[i].dat_out.eq(1)
-                        )
-
-                    # SMA RX Reg, allow direct (and slow...) visualization of SMA IOs.
-                    for i in range(io_data_width):
-                        self.sync += self.input.status[i].eq(sma_pads[i].dat_in)
-
-                    # SMA RX Data Pipeline.
-                    self.rx_converter = stream.Converter(io_data_width, pcie_data_width)
-                    self.comb += self.rx_converter.sink.valid.eq(1)
-                    for i in range(io_data_width):
-                        self.sync += self.rx_converter.sink.data[i].eq(sma_pads[i].dat_in)
-                    self.comb += self.rx_converter.source.connect(self.source)
-
-            self.smas = SMAIOs(pcie_data_width=64, io_data_width=4)
-            self.comb += self.pcie_dma0.source.connect(self.smas.sink)
-            self.comb += self.smas.source.connect(self.pcie_dma0.sink)
-
 # Build --------------------------------------------------------------------------------------------
 
 def main():
@@ -239,14 +165,12 @@ def main():
     parser.add_target_argument("--flash",        action="store_true",       help="Flash bitstream.")
     parser.add_target_argument("--sys-clk-freq", default=100e6, type=float, help="System clock frequency.")
     parser.add_target_argument("--with-pcie",    action="store_true", help="Enable PCIe support.")
-    parser.add_target_argument("--with-smas",    action="store_true", help="Enable SMAs support.")
     parser.add_target_argument("--driver",       action="store_true", help="Generate PCIe driver.")
     args = parser.parse_args()
 
     soc = BaseSoC(
         sys_clk_freq = args.sys_clk_freq,
         with_pcie    = args.with_pcie,
-        with_smas    = args.with_smas,
         **parser.soc_argdict
     )
 

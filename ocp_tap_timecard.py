dd3e48121bbbf4f656272d59829052405412f825
Florent Kermarrec <florent@enjoy-digital.fr>
2023-08-01 10:45:05 +0200

    ocp_tap_timecard: Use a proper memory for PTM capabilities.

--- ocp_tap_timecard.py
+++ ocp_tap_timecard.py
@@ -141,57 +141,58 @@ class BaseSoC(SoCMini):
 
         # PTM capabilities -------------------------------------------------------------------------
 
-        comp_port = self.pcie_endpoint.crossbar.get_slave_port(address_decoder=lambda a: 0)
-
         conf_source = self.pcie_endpoint.depacketizer.conf_source
 
-        self.ptm_fsm = ptm_fsm = FSM(reset_state="IDLE")
+        self.ptm_conf_fsm = ptm_conf_fsm = FSM(reset_state="IDLE")
 
-        tag    = Signal(8)
-        req_id = Signal(16)
-        reg    = Signal(10)
+        mem = Memory(32, 3, init=[
+            0x0001_001f, # 0x00 : PTM Extended Capbility Header.
+            0x0000_1003, # 0x04 : PTM Capability Register: Requester/Responder capable / 16ns.
+            0x0000_0000, # 0x08 : PTM Control Register.
+        ])
+        mem_wr_port = mem.get_port(write_capable=True)
+        mem_rd_port = mem.get_port(async_read=True)
+        self.specials += mem, mem_wr_port, mem_rd_port
 
-        ptm_fsm.act("IDLE",
+        reg = Signal(10)
+        dat = Signal(32)
+
+        ptm_conf_fsm.act("IDLE",
             If(conf_source.valid,
-                NextState("CONF")
-            )
-        )
-        ptm_fsm.act("CONF",
-            conf_source.ready.eq(1),
-            If(conf_source.valid & conf_source.last,
-                NextValue(tag, conf_source.tag),
-                NextValue(req_id, conf_source.req_id),
-                NextValue(reg,   Cat(conf_source.register_no, conf_source.ext_reg)),
                 If(conf_source.we,
-                    NextState("IDLE")
+                    NextState("WRITE-MEM")
                 ).Else(
-                    NextState("COMP")
+                    NextState("READ-MEM")
                 )
             )
         )
-        ptm_fsm.act("COMP",
+        self.comb += reg.eq((Cat(conf_source.register_no, conf_source.ext_reg) - 0x6B))
+        ptm_conf_fsm.act("WRITE-MEM",
+            conf_source.ready.eq(1),
+            mem_wr_port.adr.eq(reg),
+            mem_wr_port.we.eq(1),
+            mem_wr_port.dat_w.eq(conf_source.dat),
+            NextState("IDLE")
+        )
+        ptm_conf_fsm.act("READ-MEM",
+            mem_rd_port.adr.eq(reg),
+            NextValue(dat, mem_rd_port.dat_r),
+            NextState("SEND-COMPLETION")
+        )
+        comp_port = self.pcie_endpoint.crossbar.get_slave_port(address_decoder=lambda a: 0)
+        ptm_conf_fsm.act("SEND-COMPLETION",
             comp_port.source.valid.eq(1),
             comp_port.source.first.eq(1),
             comp_port.source.last.eq(1),
             comp_port.source.len.eq(1),
             comp_port.source.err.eq(0),
-            comp_port.source.tag.eq(tag),
+            comp_port.source.tag.eq(conf_source.tag),
             comp_port.source.adr.eq(0),
             comp_port.source.cmp_id.eq(self.pcie_endpoint.phy.id),
-            comp_port.source.req_id.eq(req_id),
-            # PCI Express Extended Capability Header.
-            If(reg == 0x6b,
-                comp_port.source.dat.eq(0x0001_001f),
-            ),
-            # PTM Capability Register.
-            If(reg == 0x6c,
-                comp_port.source.dat.eq(0x0000_0803), # Requester/Responder capable / 8ns.
-            ),
-            # PTM Control Register.
-            If(reg == 0x6d,
-                comp_port.source.dat.eq(0x0000_0000), # TODO.
-            ),
+            comp_port.source.req_id.eq(conf_source.req_id),
+            comp_port.source.dat.eq(dat),
             If(comp_port.source.valid & comp_port.source.ready,
+                conf_source.ready.eq(1),
                 NextState("IDLE")
             )
         )

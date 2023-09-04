bde08a9f3747d182cbc56808f0f3f52398191a17
Florent Kermarrec <florent@enjoy-digital.fr>
2023-09-04 19:06:43 +0200

    test_decoding: Add TX/RX support and binary export.

--- test_decoding.py
+++ test_decoding.py
@@ -7,41 +7,89 @@ from litex.gen import *
 
 from litex.soc.interconnect import stream
 
-from dumps.dump001 import *
+from dumps.dump002 import *
 
 from gateware.serdes import RXDatapath
 from gateware.scrambling import Descrambler
 
-def data_generator(dut, length=4096):
-    yield dut.source.ready.eq(1)
-    rx_data = dump["s7pciephy_debug_tx_data"][:length:2]
-    rx_ctrl = dump["s7pciephy_debug_tx_ctl"][:length:2]
+def rx_data_generator(dut, length=8192-1024):
+    rx_data = dump["s7pciephy_debug_rx_data"][:length:2]
+    rx_ctrl = dump["s7pciephy_debug_rx_ctl"][:length:2]
     for data, ctrl in zip(rx_data, rx_ctrl):
-        yield dut.sink.data.eq(data)
-        yield dut.sink.ctrl.eq(ctrl)
+        yield dut.rx_sink.data.eq(data)
+        yield dut.rx_sink.ctrl.eq(ctrl)
         yield
+
+@passive
+def rx_data_checker(dut, length=4096):
+    yield dut.rx_source.ready.eq(1)
+    while (yield dut.rx_source.ctrl) != 0xf:
+        yield
+    f = open("rx_data.bin", "wb")
+    while True:
+        if (yield dut.rx_source.valid):
+            f.write((yield dut.rx_source.data).to_bytes(4, byteorder="little"))
+        yield
+
+def tx_data_generator(dut, length=8192-1024):
+    tx_data = dump["s7pciephy_debug_tx_data"][:length:2]
+    tx_ctrl = dump["s7pciephy_debug_tx_ctl"][:length:2]
+    for data, ctrl in zip(tx_data, tx_ctrl):
+        yield dut.tx_sink.data.eq(data)
+        yield dut.tx_sink.ctrl.eq(ctrl)
+        yield
+
+@passive
+def tx_data_checker(dut, length=4096):
+    yield dut.tx_source.ready.eq(1)
+    while (yield dut.tx_source.ctrl) != 0xf:
+        yield
+    f = open("tx_data.bin", "wb")
+    while True:
+        if (yield dut.tx_source.valid):
+            f.write((yield dut.tx_source.data).to_bytes(4, byteorder="little"))
+        yield
+
 
 class DecodingDUT(LiteXModule):
     def __init__(self):
-        self.sink   = stream.Endpoint([("data", 16), ("ctrl", 2)])
-        self.source = stream.Endpoint([("data", 32), ("ctrl", 4)])
+        self.rx_sink   = stream.Endpoint([("data", 16), ("ctrl", 2)])
+        self.rx_source = stream.Endpoint([("data", 32), ("ctrl", 4)])
+
+        self.tx_sink   = stream.Endpoint([("data", 16), ("ctrl", 2)])
+        self.tx_source = stream.Endpoint([("data", 32), ("ctrl", 4)])
 
         # # #
 
-        self.datapath    = RXDatapath(phy_dw=16)
-        self.descrambler = Descrambler()
+        # RX.
+        self.rx_datapath    = RXDatapath(phy_dw=16)
+        self.rx_descrambler = Descrambler()
         self.comb += [
-            self.datapath.sink.valid.eq(1),
-            self.datapath.sink.data.eq(self.sink.data),
-            self.datapath.sink.ctrl.eq(self.sink.ctrl),
-            self.datapath.source.connect(self.descrambler.sink),
-            self.descrambler.source.connect(self.source),
+            self.rx_datapath.sink.valid.eq(1),
+            self.rx_datapath.sink.data.eq(self.rx_sink.data),
+            self.rx_datapath.sink.ctrl.eq(self.rx_sink.ctrl),
+            self.rx_datapath.source.connect(self.rx_descrambler.sink),
+            self.rx_descrambler.source.connect(self.rx_source),
+        ]
+
+        # TX.
+        self.tx_datapath    = RXDatapath(phy_dw=16)
+        self.tx_descrambler = Descrambler()
+        self.comb += [
+            self.tx_datapath.sink.valid.eq(1),
+            self.tx_datapath.sink.data.eq(self.tx_sink.data),
+            self.tx_datapath.sink.ctrl.eq(self.tx_sink.ctrl),
+            self.tx_datapath.source.connect(self.tx_descrambler.sink),
+            self.tx_descrambler.source.connect(self.tx_source),
         ]
 
 class TestDecoding(unittest.TestCase):
     def test_decoding(self):
         dut        = DecodingDUT()
         generators = [
-            data_generator(dut),
+            rx_data_generator(dut),
+            tx_data_generator(dut),
+            rx_data_checker(dut),
+            tx_data_checker(dut),
         ]
         run_simulation(dut, generators, vcd_name="test_decoding.vcd")

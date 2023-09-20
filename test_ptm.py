ab68b26ee0936f199c94d3fe47657169fe55a7a3
Florent Kermarrec <florent@enjoy-digital.fr>
2023-09-20 17:28:03 +0200

    test_ptm: Add vcd dump capabilities to be able to analyze timings.

--- test_ptm.py
+++ test_ptm.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import time
+import vcd
 import argparse
 
 from litex import RemoteClient
@@ -21,7 +22,7 @@ def s_ns_to_ns(value):
 
 # Test ---------------------------------------------------------------------------------------------
 
-def test_ptm(enable=1, loops=16, delay=1e-1):
+def test_ptm(enable=1, loops=16, delay=1e-1, vcd_filename="test_ptm.vcd"):
     # Create Bus.
     bus = RemoteClient()
     bus.open()
@@ -35,19 +36,41 @@ def test_ptm(enable=1, loops=16, delay=1e-1):
     while (bus.regs.ptm_requester_status.read() & PTM_STATUS_BUSY):
         pass
 
+    # VCD Writer.
+    vcd_writer = vcd.VCDWriter(open(vcd_filename, "w"), timescale="1 ns", date="today")
+    vcd_vars = {}
+    vcd_vars["t1"]    = vcd_writer.register_var("module", "t1",    "wire", size=64)
+    vcd_vars["t2"]    = vcd_writer.register_var("module", "t2",    "wire", size=64)
+    vcd_vars["t3"]    = vcd_writer.register_var("module", "t3",    "wire", size=64)
+    vcd_vars["t4"]    = vcd_writer.register_var("module", "t4",    "wire", size=64)
+    vcd_vars["t2-t1"] = vcd_writer.register_var("module", "t2-t1", "wire", size=64)
+    vcd_vars["t4-t1"] = vcd_writer.register_var("module", "t4-t1", "wire", size=64)
+
     # Read Master Time received by PTM Requester.
+    t_start  = time.time()
     while loop < loops:
         # Latch FPGA registers.
         valid          = bus.regs.ptm_requester_status.read() & PTM_STATUS_VALID
         master_time_ns = bus.regs.ptm_requester_master_time.read()
         link_delay_ns  = bus.regs.ptm_requester_link_delay.read()
-        t1_time_ns     = s_ns_to_ns(bus.regs.ptm_requester_t1_time.read())
-        t4_time_ns     = s_ns_to_ns(bus.regs.ptm_requester_t4_time.read())
+        t2_ns = master_time_ns
+        t3_ns = master_time_ns + link_delay_ns
+        t1_ns = s_ns_to_ns(bus.regs.ptm_requester_t1_time.read())
+        t4_ns = s_ns_to_ns(bus.regs.ptm_requester_t4_time.read())
+        t_current = time.time()
+        vcd_writer.change(vcd_vars["t1"],    (t_current - t_start)*1e9, t1_ns)
+        vcd_writer.change(vcd_vars["t2"],    (t_current - t_start)*1e9, t2_ns)
+        vcd_writer.change(vcd_vars["t3"],    (t_current - t_start)*1e9, t3_ns)
+        vcd_writer.change(vcd_vars["t4"],    (t_current - t_start)*1e9, t4_ns)
+        vcd_writer.change(vcd_vars["t2-t1"], (t_current - t_start)*1e9, abs(t2_ns - t1_ns))
+        vcd_writer.change(vcd_vars["t4-t1"], (t_current - t_start)*1e9, abs(t4_ns - t1_ns))
         r =  f"valid : {valid} "
-        r += f"master_time  (s): {master_time_ns/1e9:3.2f} "
-        r += f"link_delay  (ns): {link_delay_ns:d} "
-        r += f"t1_time      (s): {t1_time_ns/1e9:3.2f} "
-        r += f"t4_time      (s): {t4_time_ns/1e9:3.2f} "
+        r += f"t2    (s): {t2_ns/1e9:.9f} "
+        r += f"t3    (s): {t3_ns/1e9:.9f} "
+        r += f"t1    (s): {t1_ns/1e9:.9f} "
+        r += f"t4    (s): {t4_ns/1e9:.9f} "
+        r += f"t2-t1 (s): {(t2_ns - t1_ns)/1e9:.9f} "
+        r += f"t4-t1 (s): {(t4_ns - t1_ns)/1e9:.9f} "
         print(r)
         # Increment Loop.
         loop += 1
@@ -68,9 +91,10 @@ def main():
     parser.add_argument("--enable", default=1,    type=int,   help="PTM Enable.")
     parser.add_argument("--loops",  default=8,    type=int,   help="Test Loops.")
     parser.add_argument("--delay",  default=1e-0, type=float, help="Loop delay.")
+    parser.add_argument("--vcd",    default="test_ptm.vcd",   help="VCD dump file")
     args = parser.parse_args()
 
-    test_ptm(enable=args.enable, loops=args.loops, delay=args.delay)
+    test_ptm(enable=args.enable, loops=args.loops, delay=args.delay, vcd_filename=args.vcd)
 
 if __name__ == "__main__":
     main()

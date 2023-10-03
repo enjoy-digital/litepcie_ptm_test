d71b6c783b9cc9349431d313237fffffa11230bf
Florent Kermarrec <florent@enjoy-digital.fr>
2023-10-03 18:30:49 +0200

    test_time: Update.

--- test_time.py
+++ test_time.py
@@ -7,9 +7,9 @@ from litex import RemoteClient
 
 # Constants ----------------------------------------------------------------------------------------
 
-TIME_CONTROL_ENABLE   = (1 << 0)
-TIME_CONTROL_LATCH    = (1 << 1)
-TIME_CONTROL_OVERRIDE = (1 << 2)
+TIME_CONTROL_ENABLE = (1 << 0)
+TIME_CONTROL_READ   = (1 << 1)
+TIME_CONTROL_WRITE  = (1 << 2)
 
 # Test Time ----------------------------------------------------------------------------------------
 
@@ -23,33 +23,33 @@ def test_time(enable=1, loops=16):
 
     # Configure Time Controller.
 
-    bus.regs.time_controller_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_LATCH)
+    bus.regs.time_generator_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_READ)
 
     # Read Time from Time Controller.
     print("Read Time from Time Controller...")
     loop = 0
     while loop < loops:
-        r =   f"time (s): {bus.regs.time_controller_time.read()/1e9:0.9f} "
+        r =   f"time (s): {bus.regs.time_generator_read_time.read()/1e9:0.9f} "
         print(r)
         loop += 1
-        bus.regs.time_controller_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_LATCH)
+        bus.regs.time_generator_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_READ)
         time.sleep(1)
 
 
     # Override Time.
     print("Override Time to 100s...")
-    bus.regs.time_controller_override_time.write(int(100*1e9))
-    bus.regs.time_controller_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_OVERRIDE)
-    bus.regs.time_controller_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_LATCH)
+    bus.regs.time_generator_override_time.write(int(100*1e9))
+    bus.regs.time_generator_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_WRITE)
+    bus.regs.time_generator_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_READ)
 
     # Read Time from Time Controller.
     print("Read Time from Time Controller...")
     loop = 0
     while loop < loops:
-        r =  f"time (s): {bus.regs.time_controller_time.read()/1e9:0.9f} "
+        r =  f"time (s): {bus.regs.time_generator_time.read()/1e9:0.9f} "
         print(r)
         loop += 1
-        bus.regs.time_controller_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_LATCH)
+        bus.regs.time_generator_control.write(enable * TIME_CONTROL_ENABLE | TIME_CONTROL_READ)
         time.sleep(1)
 
 
